c2a61ddb08b09e4445e4442d0a2f761e44e48181
shukryzablah <shukryzablah@gmail.com>
2018-12-26 17:54:31 -0500

    added shiny app that lets you see multiple packages

---
+++ shinyApp/app.R
@@ -0,0 +1,25 @@
+library(shiny)
+library(ggplot2)
+library(stringr)
+
+ui <- fluidPage(
+  wellPanel(
+    textInput(inputId = "packages", label = "Packages:", value = "dplyr, caret")
+    ),
+  plotOutput(outputId = "downloadsPlot"),
+  textOutput(outputId = "list")
+)
+
+server <- function(input, output) {
+  
+  pkgs <- reactive({input$packages %>% str_replace_all("\\s+", "") %>% str_split(., pattern = ",") %>% unlist()})
+  
+  output$downloadsPlot <- renderPlot({
+    print(pkgs())
+    ggplot(cran_downloads(packages = pkgs(), from = "2015-01-01"),
+           aes(x = date, y = count, color = package)) + 
+      geom_line()
+  })
+}
+
+shinyApp(ui = ui, server = server)
